5ab4285f492cf45cae1d2acb57584e9663064acf
Taylor Perkins <taylor@taggstr.com>
2011-01-07 02:50:35 -0800

    Tried to get signin working. Failed miserably. Will try again later. Saving progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,28 @@ class SessionsController < ApplicationController
     @title = 'Sign In'
   end
 
+  def create
+    username = params[:session][:username]
+    password = params[:session][:password]
+    key = Base64.encode64(params[:session][:username]+':'+params[:session][:password])
+    if authenticate(key)
+      sign_in(key)
+      flash[:success] = 'You are signed in!'
+      redirect_to root_path
+      return
+    else
+      flash[:error] = 'Incorrect Username or Password'
+      redirect_to '/signin'
+      return
+    end
+  end
+
+private
+
+  def authenticate(key)
+    testKey = Base64.encode64('tufc:venom')
+    testKey == key
+  end
 
 end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  def sign_in(key)
+    cookies.signed.permanent[:remember_token]=['eboard',key]
+    @verified = 5,000
+  end
+
+  def signed_in?
+    return @verified
+  end
+
+  def sign_out
+    cookies.delete(:remember_token)
+    signed_in= false
+  end
+
 end
+
